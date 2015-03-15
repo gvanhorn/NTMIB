c0baea47e2960be1a1a1da10ab6a672d357d6ce7
Gijs van Horn <gijsvanhorn@gmail.com>
2015-03-15 23:46:59 +0100

    Binarization werkt

--- src/Binarization.java
+++ src/Binarization.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Stack;
 
 
 public class Binarization {
@@ -10,13 +11,15 @@ public class Binarization {
 	public static void main(String[] args) {
 		String input = "";
 		String output = "";
-		if(args != null && args[0].equals("-input")){
-			input = args[1];
-			System.out.println("input file: " + input);
-		}
-		if(args != null && args[2].equals("-output")){
-			output = args[3];
-			System.out.println("output file: " + output);
+		if(args != null){
+			if(args[0].equals("-input")){
+				input = args[1];
+				System.out.println("input file: " + input);
+			}
+			if(args != null && args[2].equals("-output")){
+				output = args[3];
+				System.out.println("output file: " + output);
+			}
 		}
 		
 		try{
@@ -28,57 +31,69 @@ public class Binarization {
 			String currentLine = br.readLine();
 			
 			while(currentLine != null){
-				
-				String binarized = binarize(currentLine);
-				bw.write(binarized);
+				if(currentLine.length() > 1){
+					String binarized = binarizeSosha(currentLine);
+					bw.write(binarized + "\n\n");
+				}
 				currentLine = br.readLine();
-				
 			}
-				
-
 			br.close();
 			bw.close();
 			
 		}catch(Exception e){
-			System.out.println(e.getMessage());
+			System.out.println(e);
 		}
 	}
+	
+	private static String binarizeSosha(String currentLine) {
 
-	
-	
-	private static String binarize(String currentLine) {
-		
-		/* 
-		 *for(every non-terminal in line)
-		 * 			if non terminal has more than 2 children,
-		 * 				newSymbol = create new nonterminal symbol as: @{Parent} -> {symbol for previous nonterminal}
-		 * 				add non-terminal as child newSymbol
-		 * 				add the rest of the children of non-terminal to newSymbol.
-		 * 				editedLine = replace non-terminal in currentLine with newSymbol.
-		 * 				binarize(editedLine)
-		 * 			else
-		 * 				do nothing
-		 * 
-		 * When the loop is escaped that means that none of the nonterminals has more than 2 children, so we can return currentLine;
-		 * return currentLine;
-		 */
-		
+		Stack<String> prefixes = new Stack<String>();
 		String[] splitLine = currentLine.split(" ");
-		
-		
-		return null;
-	}
+		String result = splitLine[0] + " " + splitLine[1] + " ";
+		prefixes.push("");
+		prefixes.push("(@"+ splitLine[1].replace("(", "") +"->");
+
+		for(int i = 2; i < splitLine.length; i++) {
+			String nextNode = splitLine[i];	
+			int closings = numberOfClosings(nextNode);
+			if(closings == 0 ){
+				String previous = prefixes.pop();
+				if(!isNewPrefix(previous)) {
+					result += previous + " ";
+				} 
 	
-	private static boolean nonTerminal(String s){
-		if(s.charAt(0) == '('){
-			return true;
-		}else{
-			return false;
+				result += nextNode +" ";
+				prefixes.push(previous + "_" +nextNode.replace("(", ""));
+				prefixes.push("(@"+nextNode.replace("(", "")+"->");
+			} else {
+				result += nextNode;
+				prefixes.pop();
+				for(int c = 1; c < closings; c++){
+					String previous = prefixes.pop();
+					for (int cc = 0; cc < numOfCharInString("_", previous)-1; cc++) {
+						result += ")";
+					}
+				}
+				result += " ";
+			}
 		}
+		return result;
 	}
-	
 
 
+	private static int numberOfClosings(String string){
+		return string.length() - string.replace(")", "").length();
+	}
+
+
+	private static int numOfCharInString(String c, String string){
+		return string.length() - string.replace(c, "").length();
+	}
+
+
+	private static boolean isNewPrefix(String prefix) {
+		return prefix.charAt(prefix.length()-1) == '>';
+	}
 }
 
 
